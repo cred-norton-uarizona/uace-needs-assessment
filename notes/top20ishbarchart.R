e33040290ced7bb9d64fa23fbc9271dc92b18068
Eric R. Scott <scottericr@gmail.com>
2023-02-24 15:14:23 -0500

    get code for bar plot working

--- notes/top20ishbarchart.R
+++ notes/top20ishbarchart.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(readxl)
 
-data <- read_excel("C:/Users/Terrace Ewinghill/Box/Cooperative Extension Needs Assessment 2022/CRED - Incubator Collaboration/Data without zips.xlsx")
+data <- read_excel("/Users/ericscott/Library/CloudStorage/Box-Box/CRED - Incubator Collaboration/Data without zips.xlsx")
 
 
 # % Extremely or Very Important Selected, Top 30 for Now-------
@@ -11,10 +11,10 @@ data <- read_excel("C:/Users/Terrace Ewinghill/Box/Cooperative Extension Needs A
 # We want to sort in descending (largest to smallest) by the tenths place
 # and then show items that tied for 20th place when rounded to the ones place.
 
-I keep getting this error.  Can you help fix my code?
-
-  Error in layer(data = data, mapping = mapping, stat = "identity", geom = GeomCol,  : 
-                   object 'Description' not found
+# I keep getting this error.  Can you help fix my code?
+# 
+#   Error in layer(data = data, mapping = mapping, stat = "identity", geom = GeomCol,  : 
+#                    object 'Description' not found
 
 data_Evimp <- data %>% group_by(COUNTY) %>%
 summarize(across(
@@ -32,7 +32,7 @@ data_Evimp <- data_Evimp %>% pivot_longer(-COUNTY,
  #  select(Metric, Percentage, COUNTY)
 
 
-labels<- read_excel("C:/Users/Terrace Ewinghill/Box/Cooperative Extension Needs Assessment 2022/CRED - Incubator Collaboration/Labels for the content areas in survey.xlsx")
+labels<- read_excel("/Users/ericscott/Library/CloudStorage/Box-Box/CRED - Incubator Collaboration/Labels for the content areas in survey.xlsx")
 
 labels <- labels %>% 
   mutate(Topic = case_when(
@@ -48,20 +48,29 @@ labels <- labels %>%
 data_joined <- left_join(data_Evimp, labels) %>% 
   select(COUNTY, Topic, Metric, Description, Percentage)
 
+colnames(data_joined)
+
 print(data_joined)
 
 # I think that the mistake that I've made here is that I need to do this work connected to the full data frame and not a cut
 # This isn't going to help us, because we won't have access to other data for filters
 
+plot_df <- data_joined |> 
+  arrange(desc(Percentage)) |> 
+  ungroup() |> 
+  mutate(Percentage = round(Percentage)/100) |> 
+  filter(Percentage >= nth(Percentage, 20)) |> 
+  mutate(row = 1:n())
 
-ggplot(data_joined, aes(x = Description, y = Percentage)) + 
-  geom_col(fill = Description, width = 0.7) +
+ggplot(plot_df, aes(x = row, y = Percentage)) + 
+  geom_col(aes(fill = Description), width = 0.7, ) +
   geom_text(aes(label = scales::percent(Percentage, accuracy = 1)), vjust = 0.5, hjust = 1.2, color = "white", size = 4) +
   scale_y_continuous(expand = c(0, 0)) +
   coord_flip() +
   labs(title = "Top Priorities",
        subtitle = "Percent of respondents who selected 'extremely' or 'very' important") +
   theme(
+    legend.position = "none",
     plot.title = element_text(size = 18, margin = margin(10, 0, 0, 0)),
     plot.subtitle = element_text(size = 12, margin = margin(10, 0, 10, 0), color = "gray"),
     panel.background = element_rect(fill = NA),
@@ -69,7 +78,8 @@ ggplot(data_joined, aes(x = Description, y = Percentage)) +
     axis.ticks = element_blank(),
     axis.title = element_blank(),
     axis.text.x = element_blank(),
-    axis.text.y = element_text(size = 11, margin = margin(0, 5, 0, 0)),
+    # axis.text.y = element_text(size = 11, margin = margin(0, 5, 0, 0)),
+    axis.text.y = element_blank()
   )
 
 # I am getting an error that 'Description' doesn't exist
