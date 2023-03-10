55b329066074e0951b7691710a2d3c9ab46c5e1a
ewinghill <terrace@arizona.edu>
2023-03-10 14:15:25 -0700

    Moved items to global and took out code that was incorporated into the cleaning script

---
+++ dashboard/global.R
@@ -0,0 +1,22 @@
+library(shiny)
+library(mapproj)
+library(maps)
+library(tidyverse)
+library(pins)
+library(shinyWidgets)
+
+
+
+board <- board_connect()
+data <- pin_read(board, "terrace/uace-na")
+
+#read in data
+# labels <- read.csv("data/labels.csv")
+# labels <- read.csv(file.path("..", "data", "labels.csv"))
+labels <- read.csv(here::here("data", "labels.csv"))
+
+
+# This is taking the following columns and recoding them as "Yes" and No" rather than 1, 0
+data <- data %>% mutate(across(
+  c(CE_USER, CE_EXPOSED, Low_Income_FPL_100, Low_Income_FPL_185),
+  function(x) ifelse(x == 1, "Yes", "No")))
--- dashboard/server.R
+++ dashboard/server.R
@@ -7,22 +7,6 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(mapproj)
-library(maps)
-library(tidyverse)
-library(pins)
-library(shinyWidgets)
-
-
-#read in data
-
-labels <- read.csv(here::here("data", "labels.csv"))
-
-board <- board_connect()
-data <- pin_read(board, "terrace/raw_data")
-
-
 az_counties <- map_data("county", region = "arizona")|>
   mutate(COUNTY = str_to_title(subregion))
 
@@ -35,39 +19,10 @@ az_counties <- map_data("county", region = "arizona")|>
 counties <- c("All", unique(data$COUNTY))
 topics <- unique(labels$Topic)
 
-# Create the age brackets
-data <- data %>% mutate(AGE = case_when(
-  DEM_9 >= 14 & DEM_9 <= 24 ~ "14-24 years old",
-  DEM_9 >= 25 & DEM_9 <= 39 ~ "25-39 years old",
-  DEM_9 >= 40 & DEM_9 <= 54 ~ "40-54 years old",
-  DEM_9 >= 55 & DEM_9 <= 64 ~ "55-64 years old",
-  DEM_9 >= 65 ~ "65 years and older"
-))
 
 
 # data %>% count(AGE)
 # Ask RG if this works for her and if these numbers look like what she found
-
-# Calculate 185% FPL using the variabled DEM_13 (Household income) and DEM_5 (number of people living in household)
-data <- data %>% 
-  mutate(Low_Income_FPL = case_when(
-    DEM_5 == 1 & DEM_13 <= 26973 ~ 1,
-    DEM_5 == 2 & DEM_13 <= 36482 ~ 1,
-    DEM_5 == 3 & DEM_13 <= 45991 ~ 1,
-    DEM_5 == 4 & DEM_13 <= 55500 ~ 1,
-    DEM_5 == 5 & DEM_13 <= 65009 ~ 1,
-    DEM_5 == 6 & DEM_13 <= 74518 ~ 1,
-    DEM_5 == 7 & DEM_13 <= 84027 ~ 1,
-    DEM_5 == 8 & DEM_13 <= 93536 ~ 1,
-    DEM_5 == 9 & DEM_13 <= 103045 ~ 1,
-    DEM_5 == 10 & DEM_13 <= 112554 ~ 1,
-    DEM_5 == 11 & DEM_13 <= 122063 ~ 1,
-    DEM_5 == 12 & DEM_13 <= 131572 ~ 1,
-    DEM_5 == 13 & DEM_13 <= 141081 ~ 1,
-    DEM_5 == 14 & DEM_13 <= 150590 ~ 1,
-    TRUE ~ 0 
-  ))
-
 
 
 # Define server logic required to draw a histogram
@@ -101,8 +56,8 @@ function(input, output, session) {
     module = selectizeGroupServer,
     id = "my-filters",
     data = data,
-    vars = c("COUNTY", "LIVE_V3", "GENDER", # "Race_Ethnicity", 
-             "AGE", "DEM_11", "Low_Income_FPL", "CE_EXPOSED", "CE_USER"), #add new filters here by adding column name in quotes
+    vars = c("COUNTY", "LIVE_V3", "UserLanguage", "GENDER", 
+             "AGE", "DEM_11", "Low_Income_FPL_100", "CE_EXPOSED", "CE_USER"), #add new filters here by adding column name in quotes
     inline = FALSE
   )
   
