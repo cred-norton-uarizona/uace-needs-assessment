aca4cedfea4f7fdd7c9cd5323be8fbfbb0f3e123
Eric R. Scott <scottericr@gmail.com>
2023-03-21 13:02:23 -0400

    remove old code

--- dashboard/server.R
+++ dashboard/server.R
@@ -27,31 +27,6 @@ topics <- unique(labels$Topic)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
-  
-  # data_Evimp <- data %>% group_by(SELECTED INPUTS????) %>%
-  #   summarize(across(
-  #     ends_with("Evimp"),
-  #     ~sum(.x == 1, na.rm = TRUE)/n())*100),
-  # 
-  # data_Evimp <- data_Evimp %>% pivot_longer(-COUNTY,
-  #                                           names_to = "Metric",
-  #                                           values_to = "Percentage"
-  # ) %>%
-  #   group_by(SELECTED INPUTS????) %>% 
-  #   arrange(desc(Percentage)) %>% 
-  #   mutate(Metric = gsub("_EVimp", "", Metric))%>%
-  #   slice(1:30),
-  # 
-  # output$top_priorities <- renderPlot({
-  #   priorities <- top_priorities  %>% 
-  #     mutate(selected = ifelse(COUNTY == input$select_county, TRUE, FALSE))
-  #   
-  #   ggplot(data_Evimp, aes(x = Metric, y = Percentage)) +
-  #     geom_col(fill = )
-  #   
-  #   
-  # })
   
   top_20_filtered <- callModule(
     module = selectizeGroupServer,
