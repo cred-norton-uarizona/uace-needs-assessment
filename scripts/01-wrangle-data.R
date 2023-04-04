7b81a53c7e2dfb3a486ac1ac1141b712239c3e57
Eric R. Scott <scottericr@gmail.com>
2023-04-04 17:55:19 -0400

    filter to bare minimum columns

--- scripts/01-wrangle-data.R
+++ scripts/01-wrangle-data.R
@@ -2,6 +2,23 @@ library(tidyverse)
 library(readxl)
 library(pins)
 
+labels <- read_excel("C:/Users/Terrace Ewinghill/Box/Cooperative Extension Needs Assessment 2022/CRED - Incubator Collaboration/Labels for the content areas in survey.xlsx")
+
+labels <- labels %>%
+  mutate(Topic = case_when(
+    str_detect(Metric, "FCHS_") ~ "Health and Well-Being",
+    str_detect(Metric, "YD_") ~ "Education",
+    str_detect(Metric, "AG_") ~ "Agriculture",
+    str_detect(Metric, "NR_") ~ "Natural Resources",
+    str_detect(Metric, "CED_") ~ "Community and Economic Development",
+    TRUE ~ 'NA'
+  ))
+
+write_csv(labels, "data/labels.csv")
+write_csv(labels, "dashboard/labels.csv")
+
+
+
 data <- 
   read_csv("//corner2.sfcs.cals.arizona.edu/ERDU$/Shared/Projects/Cooperative Extension/Statewide Needs assessment 2022/Data/clean-data/scena_survey_wrangled_3.23.23 no zip.csv") %>%
   select(
@@ -22,24 +39,13 @@ data <-
     WH,
     NR,
     ends_with("EXPERIENCE"),
-    ends_with("KNOWLEDGE")
+    ends_with("KNOWLEDGE"),
+    Bach_or_higher,
+    non_white,
+    all_of(labels$Metric)
   )
 
 board <- board_connect()
 pin_write(board, data, "uace-na", type = "arrow")
 
-labels <- read_excel("C:/Users/Terrace Ewinghill/Box/Cooperative Extension Needs Assessment 2022/CRED - Incubator Collaboration/Labels for the content areas in survey.xlsx")
-
-labels <- labels %>%
-  mutate(Topic = case_when(
-    str_detect(Metric, "FCHS_") ~ "Health and Well-Being",
-    str_detect(Metric, "YD_") ~ "Education",
-    str_detect(Metric, "AG_") ~ "Agriculture",
-    str_detect(Metric, "NR_") ~ "Natural Resources",
-    str_detect(Metric, "CED_") ~ "Community and Economic Development",
-    TRUE ~ 'NA'
-  ))
-
-write_csv(labels, "data/labels.csv")
-write_csv(labels, "dashboard/labels.csv")
 
