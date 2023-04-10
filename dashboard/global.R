7b57d03228d14e51c01ef984dc064422f8667052
Jessica Guo <jessicaguo@email.arizona.edu>
2023-04-10 13:12:26 -0700

    Improve graphics with Cairo on Posit connect

--- dashboard/global.R
+++ dashboard/global.R
@@ -2,6 +2,8 @@ library(shiny)
 library(shinydashboard)
 library(shinyWidgets)
 library(plotly)
+library(Cairo)
+options(shiny.usecairo = TRUE)
 
 # read in data needed for both ui and server
 labels <- read.csv("labels.csv")
