5fb711add3186b5c8642623a027353150ff490b6
ewinghill <terrace@arizona.edu>
2023-04-10 11:22:34 -0700

    Pinned new data

--- scripts/01-wrangle-data.R
+++ scripts/01-wrangle-data.R
@@ -4,7 +4,7 @@ library(readxl)
 library(pins)
 # install.packages("rsconnect")
 
-data <- read.csv("C:/Users/Terrace Ewinghill/Box/Cooperative Extension Needs Assessment 2022/CRED - Incubator Collaboration/scena_survey_wrangled_4.6.2023 no zip.csv")
+data <- read.csv("C:/Users/Terrace Ewinghill/Box/Cooperative Extension Needs Assessment 2022/CRED - Incubator Collaboration/scena_survey_wrangled_4.7C.2023.csv")
 
 
 
